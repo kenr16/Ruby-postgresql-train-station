ceb1862131bd1e58329e293a2b5b956bca414a71
Ken Rutan and Nick Powell <nick@>
2017-05-03 11:19:12 -0700

    add specs to stops

--- lib/stops.rb
+++ lib/stops.rb
@@ -1,11 +1,12 @@
 class Stop
-  attr_accessor(:id, :name, :city_id, :train_id)
+  attr_accessor(:id, :name, :city_id, :train_id, :time)
 
   define_method(:initialize) do |attributes|
     @id = attributes.fetch(:id)
     @name = attributes.fetch(:name)
     @city_id = attributes.fetch(:city_id)
     @train_id = attributes.fetch(:train_id)
+    @time = attributes.fetch(:time)
   end
 
   define_singleton_method(:all) do
@@ -13,11 +14,24 @@ class Stop
     stops = []
     returned_stops.each() do |stop|
       name = stop.fetch("name")
-      birthday = stop.fetch("birthday")
-      doctor_id = stop.fetch("doctor_id").to_i()
-      stops.push(Patient.new({:name => name, :birthday => birthday, :doctor_id => doctor_id}))
+      city_id = stop.fetch("city_id").to_i
+      train_id = stop.fetch("train_id").to_i
+      time = stop.fetch("time")
+      id = stop.fetch("id").to_i
+      stops.push(Stop.new({:id => id, :name => name, :city_id => city_id, :train_id => train_id, :time => time}))
     end
     stops
+  end
+
+  define_method(:==) do |another_stop|
+    self.name().==(another_stop.name())
+    .&(self.id().==(another_stop.id()))
+    .&(self.time().==(another_stop.time()))
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stops (name, city_id, train_id, time) VALUES ('#{@name}', #{@city_id}, '#{@train_id}', '#{@time}') RETURNING id;")
+    @id = result.first.fetch("id").to_i()
   end
 
 end
--- spec/stops_spec.rb
+++ spec/stops_spec.rb
@@ -8,13 +8,52 @@ describe(Stop) do
     end
   end
 
-  # describe('#==') do
-  #   it("is the same patient if it has the same name") do
-  #     patient1 = Patient.new({:name => "Mark Jones", :doctor_id => 1, :birthday => "2008-11-11"})
-  #     patient2 = Patient.new({:name => "Mark Jones", :doctor_id => 1, :birthday => "2008-11-11"})
-  #     expect(patient1==patient2).to(eq(true))
-  #   end
-  # end
+  describe('#==') do
+    it("is the same stop if it has the same name") do
+      stop1 = Stop.new({:id => 1, :name => "Blue", :city_id => 1, :train_id => 1, :time => '2017-01-01 00:05:00'})
+      stop2 = Stop.new({:id => 2, :name => "Red", :city_id => 2, :train_id => 3, :time => '2017-01-01 00:05:30'})
+      stop3 = Stop.new({:id => 1, :name => "Blue", :city_id => 1, :train_id => 1, :time => '2017-01-01 00:05:00'})
+      expect(stop1==stop3).to(eq(true))
+    end
+  end
+
+  describe('#save') do
+    it("adds a stop to the array of saved stops") do
+      test_stop = Stop.new({:id => 27, :name => "Blue", :city_id => 1, :train_id => 1, :time => '2017-01-01 00:05:00'})
+      test_stop.save()
+      expect(Stop.all()).to(eq([test_stop]))
+    end
+  end
+
+  describe("#id") do
+    it("sets stops's ID when you save it") do
+      stop = Stop.new({:id => nil, :name => "Blue", :city_id => 1, :train_id => 1, :time => '2017-01-01 00:05:00'})
+      stop.save()
+      expect(stop.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#name') do
+    it("lets you view the stop's name") do
+      stop = Stop.new({:id => nil, :name => "Blue", :city_id => 1, :train_id => 1, :time => '2017-01-01 00:05:00'})
+      expect(stop.name()).to(eq("Blue"))
+    end
+  end
+
+  describe('#city_id') do
+    it("lets you view the stop's city_id") do
+      stop = Stop.new({:id => nil, :name => "Blue", :city_id => 101, :train_id => 1, :time => '2017-01-01 00:05:00'})
+      expect(stop.city_id()).to(eq(101))
+    end
+  end
+
+  describe('#train_id') do
+    it("lets you view the stop's train_id") do
+      stop = Stop.new({:id => nil, :name => "Blue", :city_id => 101, :train_id => 16, :time => '2017-01-01 00:05:00'})
+      expect(stop.train_id()).to(eq(16))
+    end
+  end
+
 
 
 end
